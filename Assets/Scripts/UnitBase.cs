73764eb58d3b39dc33be169f0fc64c81568e0c1a
bsw9807 <43857748+bsw9807@users.noreply.github.com>
2022-12-22 22:25:55 +0900

    asdasd

--- Assets/Scripts/UnitBase.cs
+++ Assets/Scripts/UnitBase.cs
@@ -6,41 +6,34 @@ using UnityEngine.UI;
 
 public class UnitBase : MonoBehaviour
 {
-    public Animator animator;
-    public GameObject prefHpBar;
-    public GameObject canvas;
-    public float hpBar_h;
-    RectTransform hpBar;
-    Image currentHPbar;
-
     public List<GameObject> FoundObjects;
     public GameObject target;
-    float tDis;
-
-    float atkDelay;
-
+    public float tDis;
     public Vector2 dirVec;
 
-    #region UnitStatus
+    public Animator animator;
+    public GameObject prefHpBar;
+    public GameObject prefMpBar;
+    public GameObject canvas;
+    public float hpBar_h;
+    public float mpBar_h;
+    RectTransform hpBar;
+    RectTransform mpBar;
+    Image currentHPbar;
+    Image currentMPbar;
+
+    public float atkDelay;  // 공격 딜레이
+
     public string unitName; // 유닛 이름
     public float unitHP;    // 체력
     public float currentHP; // 현재 체력
+    public float unitMP;    // 마나
+    public float currentMP; // 현재 마나
     public float unitMS;    // 이동 속도
     public float unitAD;    // 공격력
     public float unitAS;    // 공격 속도
     public float unitDis;   // 공격 범위
-
-    private void SetUnitStatus(string unitName, float unitHP, float unitMS, float unitAD, float unitAS, float unitDis)
-    {
-        unitName = this.unitName;
-        unitHP = this.unitHP;
-        currentHP = this.unitHP;
-        unitMS = this.unitMS;
-        unitAD = this.unitAD;
-        unitAS = this.unitAS;
-        unitDis = this.unitDis;
-    }
-    #endregion
+    public string unitType;  // 유닛 타입
 
     public enum UnitState
     {
@@ -52,25 +45,85 @@ public class UnitBase : MonoBehaviour
         dead
     }
 
+    public enum UnitType
+    {
+        Warrior,
+        Archer,
+        Magician,
+        Priest,
+        Assassin
+    }           // 미적용
+
+    void UnitHpbar()
+    {
+        hpBar = Instantiate(prefHpBar, canvas.transform).GetComponent<RectTransform>();
+        currentHPbar = hpBar.transform.GetChild(0).GetComponent<Image>();
+    }
+
+    void UnitHpbarUpdate()
+    {
+        Vector3 hpBarPos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + hpBar_h, 0));
+        hpBar.position = hpBarPos;
+        currentHPbar.fillAmount = currentHP / unitHP;
+
+        if (currentHP <= 0)
+        {
+            unitState = UnitState.dead;
+        }
+    }
+
+    void UnitMpbar()
+    {
+        mpBar = Instantiate(prefMpBar, canvas.transform).GetComponent<RectTransform>();
+        currentMPbar = mpBar.transform.GetChild(0).GetComponent<Image>();
+    }
+
+    void UnitMpbarUpdate()
+    {
+        Vector3 mpBarPos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + mpBar_h, 0));
+        mpBar.position = mpBarPos;
+        currentMPbar.fillAmount = currentMP / unitMP;
+
+        if (currentMP <= 0)
+        {
+            unitState = UnitState.dead;
+        }
+    }
+
+    private void SetUnitStatus(string unitName, float unitHP, float unitMP, float unitMS, float unitAD, float unitAS, float unitDis, string unitType)
+    {
+        unitName = this.unitName;
+        unitHP = this.unitHP;
+        currentHP = this.unitHP;
+        unitMP = this.unitMP;
+        currentMP = this.unitMP;
+        unitMS = this.unitMS;
+        unitAD = this.unitAD;
+        unitAS = this.unitAS;
+        unitDis = this.unitDis;
+        unitType = this.unitType;
+    }
+
+    void SetAS(float AS)
+    {
+        animator.SetFloat("AttackSpeed", AS);
+        unitAS = AS;
+    }
+
     void Start()
     {
-        #region 01_Warrior_Status
-        if (name.Equals("01_Warrior"))
-        {
-            SetUnitStatus("01_Warrior", 1000, 50, 100, 1, 10);
-        }
-        #endregion
-
-        UnitHP();
+        UnitHpbar();
+        UnitMpbar();
         SetAS(unitAS);
         FindTarget();
     }
 
     void Update()
     {
-        UnitHPUpdate();
-        CheckState();
+        UnitHpbarUpdate();
+        UnitMpbarUpdate();
         UnitAI();
+        CheckState();
     }
 
     public UnitState unitState = UnitState.idle;
@@ -81,31 +134,43 @@ public class UnitBase : MonoBehaviour
         switch (unitState)
         {
             case UnitState.idle:
+                #region Anim
                 animator.SetFloat("Runstate", 0f);
+                #endregion
                 break;
 
             case UnitState.run:
+                #region Anim
                 animator.SetFloat("Runstate", 0.5f);
+                #endregion
                 break;
 
             case UnitState.attack:
+                #region Anim
                 animator.SetTrigger("Attack");
                 animator.SetFloat("AttackState", 0.0f);
                 animator.SetFloat("NormalState", 0.0f); // 0.0: Sword // 0.5: Bow // 1.0: Magic
+                #endregion
                 break;
 
             case UnitState.skill:
+                #region Anim
                 animator.SetTrigger("Attack");
                 animator.SetFloat("AttackState", 1.0f);
                 animator.SetFloat("NormalState", 0.0f);
+                #endregion
                 break;
 
             case UnitState.stun:
+                #region Anim
                 animator.SetFloat("Runstate", 1.0f);
+                #endregion
                 break;
 
             case UnitState.dead:
+                #region Anim
                 animator.SetTrigger("Die");
+                #endregion
                 break;
         }
     }
@@ -136,43 +201,21 @@ public class UnitBase : MonoBehaviour
                 onDie();
                 break;
         }
-    }
 
-    void UnitHP()
-    {
-        hpBar = Instantiate(prefHpBar, canvas.transform).GetComponent<RectTransform>();
-        currentHPbar = hpBar.transform.GetChild(0).GetComponent<Image>();
-    }
-
-    void UnitHPUpdate()
-    {
-        Vector3 hpBarPos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + hpBar_h, 0));
-        hpBar.position = hpBarPos;
-        currentHPbar.fillAmount = currentHP / unitHP;
-
-        if(currentHP <= 0)
-        {
-            onDie();
-        }
-    }
-
-    void SetAS(float AS)
-    {
-        animator.SetFloat("AttackSpeed", AS);
-        unitAS = AS;
     }
 
     void UnitAI()
     {
-        atkDelay -= Time.deltaTime;
-        if(atkDelay < 0) atkDelay = 0;
-
+        atkDelay += Time.deltaTime;
+        if (atkDelay >= unitAS) atkDelay = 0;
+        
         float dis = Vector3.Distance(transform.position, target.transform.position);
 
-        if (atkDelay == 0 && CheckTarget())
+
+        if (CheckTarget())      // 타겟이 있으면
         {
             FaceTarget();
-            if (dis <= unitAD)
+            if (dis <= unitDis && atkDelay == 0)
             {
                 unitState = UnitState.attack;
             }
@@ -185,6 +228,8 @@ public class UnitBase : MonoBehaviour
         {
             unitState = UnitState.idle;
         }
+
+        SetState(unitState);
     }
 
     void FindTarget()
@@ -227,7 +272,6 @@ public class UnitBase : MonoBehaviour
     void AttackTarget() //타겟 공격
     {
         target.GetComponent<UnitBase>().currentHP -= unitAD;
-        unitState = UnitState.attack;
     }
 
     void MoveToTarget() //타겟으로 이동
@@ -235,22 +279,25 @@ public class UnitBase : MonoBehaviour
         Vector2 dir = (Vector2)(target.transform.localPosition - transform.position);
         dirVec = dir.normalized;
         transform.position += (Vector3)dirVec * unitMS * Time.deltaTime;
-        unitState = UnitState.run;
     }
 
     void onDie() // 유닛 사망 후 실행
     {
+        Destroy(gameObject, 1);
+        Destroy(hpBar.gameObject, 1);
+        Destroy(mpBar.gameObject, 1);
         unitState = UnitState.dead;
-        Destroy(gameObject, 3);
-        Destroy(hpBar.gameObject, 3);
     }
 
-    bool CheckTarget() //타겟 존재 여부 체크
+    bool CheckTarget()
     {
-        if (target == null) return false;
-        if(target.GetComponent<UnitBase>().unitState == UnitState.dead) return false;
-        if (!target.activeInHierarchy) return false;
-
+        if (target == null)
+            return false;
+        if (target.GetComponent<UnitBase>().unitState == UnitState.dead)
+            return false;
+        if (!target.activeInHierarchy) 
+            return false;
+        
         return true;
     }
 }
